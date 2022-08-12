d03e08b5513be611c6a591caf9d496beae25a0c8
jungeun-p <mrk7753@naver.com>
2022-08-12 19:54:13 +0900

    [ modify ] Link and Router from Routing

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,3 +1,8 @@
+import NavBar from "../components/NavBar";
+
 export default function Jin(){
-    return "about us";
+    return <div>
+        <NavBar />
+        <h1>About</h1>
+    </div>
 }
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
-    const [counter, setCounter] = useState(0);
     return(
         <div>
-            <h1>Hello {counter}</h1>
-            <button onClick={() => setCounter((prev) => prev+2)}>Add</button>
+            <NavBar />
+            <p>Hello</p>
         </div>
     )
-} 
+}
