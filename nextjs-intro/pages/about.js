bc141cbb760510575957090ce5db39f32ae6b7ca
jungeun-p <mrk7753@naver.com>
2022-08-12 21:51:24 +0900

    [ modify ] Styled jsx

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -3,6 +3,12 @@ import NavBar from "../components/NavBar";
 export default function Jin(){
     return <div>
         <NavBar />
-        <h1>About</h1>
+        <h1 className="active">About</h1>
+        <style jsx>{`
+            a {
+                color: blue;
+
+            }
+        `}</style>
     </div>
 }
