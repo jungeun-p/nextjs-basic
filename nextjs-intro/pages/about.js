b808f8a67c4f83dfd783f587e99c255afb046a1a
jungeun-p <mrk7753@naver.com>
2022-08-15 10:58:52 +0900

    [ modify ] styled jsx

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -4,11 +4,5 @@ export default function Jin(){
     return <div>
         <NavBar />
         <h1 className="active">About</h1>
-        <style jsx>{`
-            a {
-                color: blue;
-
-            }
-        `}</style>
     </div>
 }
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -4,7 +4,12 @@ export default function Home() {
     return(
         <div>
             <NavBar />
-            <p>Hello</p>
+            <h1>Hello</h1>
+            <style jsx global>{`
+                a{
+                    color: white;
+                }
+            `}</style>
         </div>
     )
 }
