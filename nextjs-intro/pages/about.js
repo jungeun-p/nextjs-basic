5b9ad7eeca7569cd61af5506a80d16e5f6982238
jungeun-p <mrk7753@naver.com>
2022-08-15 12:16:39 +0900

    [ modify ] Component patterns

--- nextjs-intro/pages/about.js
+++ nextjs-intro/pages/about.js
@@ -1,7 +1,8 @@
-import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
-export default function Jin(){
+export default function About(){
     return <div>
+        <Seo title="About" />
         <h1 className="active">About</h1>
     </div>
 }
