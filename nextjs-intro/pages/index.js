5c4b4612449a4b0799b7dd1cbea50335b7240e6c
jungeun-p <mrk7753@naver.com>
2022-08-16 13:15:02 +0900

    [ modify ] getServerSideProps

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-        (async() => {
-            const { results } = await (await fetch("/api/movies")).json(); 
-            setMovies(results);
-        })();        
-    }, []);
+export default function Home({ results }) {
+    // const [movies, setMovies] = useState();
+    // useEffect(() => {
+    //     (async() => {
+    //         const { results } = await (await fetch(`/api/movies`)).json(); 
+    //         setMovies(results);
+    //     })();        
+    // }, []);
     return(
         <div>
             <div className="container">
                 <Seo title="Home" />
-                {!movies && <h4>loading...</h4>}
-                {movies?.map((movie) =>
+                {/* {!movies && <h4>loading...</h4>} */}
+                {results?.map((movie) =>
                     <div className="movie" key={movie.id}>
                         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                         <h4>{movie.original_title}</h4>
                     </div>)}
             </div>
-            <style jsx>{`
+            <style jsx>{` 
                 .container {
                     display: grid;
                     grid-template-columns: 1fr 1fr;
@@ -43,4 +43,14 @@ export default function Home() {
             `}</style>
         </div>
     );
+}
+
+// server쪽에서만 작동한다
+export async function getServerSideProps(){
+    const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+    return {
+        props: {
+            results, // props로 page에 전달
+        }
+    }
 }
